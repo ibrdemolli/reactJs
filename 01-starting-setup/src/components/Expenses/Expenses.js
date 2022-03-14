9cf98c05d239a2b46998413b3edc9bb0866d11c4
ibrdemolli <ibr.demolli@gmail.com>
2022-03-14 10:38:11 +0100

    UseState, two way binding(controlled component)

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -5,12 +5,10 @@ import "./Expenses.css";
 import { useState } from "react";
 
 function Expenses(props) {
-
   const [filteredYear, setFilteredYear] = useState("2022");
   const selectedOptionHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-  
   return (
     <div>
       <Card className="expenses">
@@ -18,26 +16,13 @@ function Expenses(props) {
           selectedValue={filteredYear}
           onSelectedOption={selectedOptionHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
